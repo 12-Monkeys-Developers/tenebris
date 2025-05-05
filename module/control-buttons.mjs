6c2e04b11adc017a39bccf50e2687d81bc172215
Christophe Laudon <ch.laudon@gmail.com>
2025-05-05 21:26:03 +0200

    Changement de la gestion du menu

--- module/control-buttons.mjs
+++ module/control-buttons.mjs
@@ -2,43 +2,38 @@
  * Menu spécifique au système
  */
 export function initControlButtons() {
-  CONFIG.Canvas.layers.tenebris = { layerClass: ControlsLayer, group: "primary" }
-
-  Hooks.on("getSceneControlButtons", (btns) => {
-    let menu = []
-
-    menu.push({
-      name: "fortune",
-      title: game.i18n.localize("TENEBRIS.Fortune.title"),
-      icon: "fa-solid fa-clover",
-      button: true,
-      onClick: () => {
-        if (!foundry.applications.instances.has("tenebris-application-fortune")) {
-          game.system.applicationFortune.render(true)
-        } else game.system.applicationFortune.close()
-      },
-    })
-
-    if (game.user.isGM) {
-      menu.push({
-        name: "gm-manager",
-        title: game.i18n.localize("TENEBRIS.Manager.title"),
-        icon: "fa-solid fa-users",
-        button: true,
-        onClick: () => {
-          if (!foundry.applications.instances.has("tenebris-application-manager")) {
-            game.system.applicationManager.render(true)
-          } else game.system.applicationManager.close()
-        },
-      })
-    }
-
-    btns.push({
+  Hooks.on("getSceneControlButtons", (controls) => {
+    const menu = {
       name: "tenebris",
       title: "Cthulhu Tenebris",
       icon: "tenebris",
-      layer: "tenebris",
-      tools: menu,
-    })
+      tools: {
+        fortune: {
+          name: "fortune",
+          title: game.i18n.localize("TENEBRIS.Fortune.title"),
+          icon: "fa-solid fa-clover",
+          toggle: true,
+          onChange: (_event, toggled) => {
+            if (!foundry.applications.instances.has("tenebris-application-fortune")) {
+              game.system.applicationFortune.render(true)
+            } else game.system.applicationFortune.close()
+          },
+        },
+        gmManager: {
+          name: "gmManager",
+          title: game.i18n.localize("TENEBRIS.Manager.title"),
+          icon: "fa-solid fa-users",
+          toggle: true,
+          visible: game.user.isGM,
+          onChange: (_event, toggled) => {
+            if (!foundry.applications.instances.has("tenebris-application-manager")) {
+              game.system.applicationManager.render(true)
+            } else game.system.applicationManager.close()
+          },
+        },
+      },
+    }
+
+    controls.tenebris = menu
   })
 }
